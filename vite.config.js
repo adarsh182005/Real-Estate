a9714855ddd36aeafe47f732a8d006ce40d5a970
Adarsh <adarshnalawade2005@gmail.com>
2025-07-16 12:18:00 +0530

    updated tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  
-  base: '/Real-Estate/', // Keep this if deploying to GitHub Pages under that repo
+  plugins: [tailwindcss(),
+    react()],
+  base: '/Real-Estate/', // Keep this for GitHub Pages
 });
 
